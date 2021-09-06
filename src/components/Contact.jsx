06877a7196b3da1e58ed845620ca17573b7df85a
jsxgod <smykoo7@gmail.com>
2021-09-06 18:45:59 +0200

    Create components

---
+++ src/components/Contact.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Contact = () => {
+  return <p>Feel free to contact us under this email: company@email.com</p>;
+};
+
+export default Contact;
---
+++ src/components/Solutions.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Solutions = () => {
+  return <p>Solutions re-imagined with your growth in mind</p>;
+};
+
+export default Solutions;
