ace38674caef83fe2e5318e615a128cec7b63983
jsxgod <smykoo7@gmail.com>
2021-09-06 23:05:55 +0200

    Add Header jsx. Add Menu component to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Menu } from ".";
 
 const Header = () => {
-  return <header></header>;
+  return (
+    <header>
+      <div className="container">
+        <div className="wrapper">
+          <div className="inner-header">
+            <div className="logo">
+              <Link to="/">COMPANY</Link>
+            </div>
+            <div className="menu-btn">
+              <button>Menu</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Menu />
+    </header>
+  );
 };
 
 export default Header;
