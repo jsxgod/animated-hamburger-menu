4b259dd1b8fb9fa1b0cbede281531b8d2c3520d8
jsxgod <smykoo7@gmail.com>
2021-09-06 23:07:07 +0200

    Add Menu jsx. Add link items to menu's unordered list.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,55 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Menu = () => {
-  return <div className="">Menu</div>;
+  return (
+    <div className="menu">
+      <div className="menu-secondary-bg-color"></div>
+      <div className="menu-layer">
+        <div className="menu-background"></div>
+        <div className="container">
+          <div className="wrapper">
+            <div className="menu-links">
+              <nav>
+                <ul>
+                  <li>
+                    <Link to="/opportunities">Opportunities</Link>
+                  </li>
+                  <li>
+                    <Link to="/solutions">Solutions</Link>
+                  </li>
+                  <li>
+                    <Link to="/contact">Contact us</Link>
+                  </li>
+                </ul>
+              </nav>
+              <div className="info">
+                <h3>Our Promise</h3>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </p>
+              </div>
+              <div className="locations">
+                Locations:
+                <span>Dallas</span>
+                <span>Austin</span>
+                <span>New York</span>
+                <span>San Francisco</span>
+                <span>Beijing</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
