eea7aafe21fb099a26e0c2164ff9088715421bc2
jsxgod <smykoo7@gmail.com>
2021-09-07 22:31:38 +0200

    Migrate the information about cities to a seperate file

---
+++ src/components/cities.js
@@ -0,0 +1,15 @@
+import dallas from '../images/dallas.webp';
+import austin from '../images/austin.webp';
+import newyork from '../images/newyork.webp';
+import sanfrancisco from '../images/sanfrancisco.webp';
+import beijing from '../images/beijing.webp';
+
+const cities = [
+    { name: "Dallas", image: dallas },
+    { name: "Austin", image: austin },
+    { name: "New York", image: newyork },
+    { name: "San Francisco", image: sanfrancisco },
+    { name: "Beijing", image: beijing },
+]
+
+export default cities;
